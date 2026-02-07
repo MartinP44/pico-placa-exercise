2b0b00c6fa6170e19fc9c5f0e3ba26397238627a
Martín Posso <martin.posso@epn.edu.ec>
2026-02-07 10:45:49 -0500

    Initial commit

---
+++ src/main/java/com/posso/martin/pico/placa/predictor/PicoPlacaPredictor.java
@@ -0,0 +1,14 @@
+
+
+package com.posso.martin.pico.placa.predictor;
+
+/**
+ *
+ * @author Martin Posso
+ */
+public class PicoPlacaPredictor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
