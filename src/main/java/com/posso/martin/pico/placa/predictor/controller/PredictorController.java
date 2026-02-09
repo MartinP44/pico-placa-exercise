188ecd22caa32824d63ce3fdd8fccd8993a18d15
Martín Posso <martin.posso@epn.edu.ec>
2026-02-08 20:18:30 -0500

    feat(PredictorController): add method to make the prediction of Pico y Placa

--- src/main/java/com/posso/martin/pico/placa/predictor/controller/PredictorController.java
+++ src/main/java/com/posso/martin/pico/placa/predictor/controller/PredictorController.java
@@ -1,9 +1,17 @@
 package com.posso.martin.pico.placa.predictor.controller;
 
-
+import com.posso.martin.pico.placa.predictor.exception.InvalidPlateException;
+import com.posso.martin.pico.placa.predictor.exception.InvalidTimeException;
+import com.posso.martin.pico.placa.predictor.exception.InvalidDateException;
+import com.posso.martin.pico.placa.predictor.model.Plate;
+import com.posso.martin.pico.placa.predictor.model.PredictionResult;
 import com.posso.martin.pico.placa.predictor.service.impl.ImplPicoPlacaService;
+import com.posso.martin.pico.placa.predictor.util.DateTimeUtil;
+import com.posso.martin.pico.placa.predictor.util.FormatValidatorUtil;
 import com.posso.martin.pico.placa.predictor.view.JFInputForm;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  *
@@ -11,13 +19,37 @@ import com.posso.martin.pico.placa.predictor.view.JFInputForm;
  */
 public class PredictorController {
 
-    private ImplPicoPlacaService picoPlacaservice;
+    private ImplPicoPlacaService picoPlacaService;
     private JFInputForm inputForm;
     
-    public PredictorController(ImplPicoPlacaService picoPlacaservice, JFInputForm inputForm) {
-        this.picoPlacaservice = picoPlacaservice;
+    public PredictorController(ImplPicoPlacaService picoPlacaService, JFInputForm inputForm) {
+        this.picoPlacaService = picoPlacaService;
         this.inputForm = inputForm;
         
+    }
+    
+    public void makePrediction(){
+        Plate plate = new Plate(inputForm.getPlate());
+        String dateS = inputForm.getDate();
+        String timeS = inputForm.getTime();
+        String numPlate = plate.getPlateNumber();
+        
+        try{
+            // Format validating
+            FormatValidatorUtil.checkPlateFormat(numPlate);
+            FormatValidatorUtil.checkDateFormat(dateS);
+            FormatValidatorUtil.checkTimeFormat(timeS);
+            
+            LocalDate date = DateTimeUtil.parseDate(dateS);
+            LocalTime time = DateTimeUtil.parseTime(timeS);
+            
+            PredictionResult result = picoPlacaService.checkPicoPlaca(numPlate, date, time);
+            
+            System.out.println(result.getMessage());
+            
+        }catch(InvalidPlateException | InvalidDateException | InvalidTimeException e){
+            System.err.println(e.getMessage());
+        }
     }
     
 }
